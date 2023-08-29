4acc21b6b129064826654a4f1d55f49e3e52825c
Garreth-Ward <garrethward1@gmail.com>
2023-08-29 11:54:49 +0100

    Added JWT authentication

--- API/Program.cs
+++ API/Program.cs
@@ -1,22 +1,27 @@
-using API.Data;
-using Microsoft.EntityFrameworkCore;
+using System.Text;
+using API.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt => 
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")); 
-});
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
-builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+app.UseCors(builder => builder.AllowAnyHeader()
+.AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
+app.UseAuthentication(); //Middleware to check if you have a token
+app.UseAuthorization(); //checks is token valid
+
 app.MapControllers();
 
 app.Run();
